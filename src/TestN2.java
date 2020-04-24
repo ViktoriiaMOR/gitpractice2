2f078f447896c1f5cf1125bfa2408957d0216edc
ViktoriiaMOR <morgonovavika@mail.ru>
2020-04-24 16:09:12 -0400

    TestN2

--- src/TestN2.java
+++ src/TestN2.java
@@ -1,7 +1,7 @@
 public class TestN2 {
     public static void main(String[] args) {
         System.out.println("create a class \n git add . \n git commit -m\"add classname.java\" \n git push" ) ;
-
+        System.out.println("refresh page");
 
 
 
