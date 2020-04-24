71faefef772f45a27b0673aeeda0d35618074f81
ViktoriiaMOR <morgonovavika@mail.ru>
2020-04-24 15:55:34 -0400

    add TestN2.java

---
+++ src/TestN2.java
@@ -0,0 +1,10 @@
+public class TestN2 {
+    public static void main(String[] args) {
+        System.out.println("create a class \n git add . \n git commit -m\"add classname.java\" \n git push" ) ;
+
+
+
+
+
+    }
+}
