aa64a8abe603d3970cdb4047de5472d9e36715fa
ViktoriiaMOR <64219552+ViktoriiaMOR@users.noreply.github.com>
2020-04-28 15:49:25 -0400

    Update HappyBirthday.java

--- src/Day_1/HappyBirthday.java
+++ src/Day_1/HappyBirthday.java
@@ -4,7 +4,7 @@ public class HappyBirthday {
     public static void main(String[] args) {
 
         System.out.println("Happy birthday!");
-
-
+        //this is added from github
+        System.out.println("Batch 17");
     }
 }
